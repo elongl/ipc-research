40ffd38bb844da8ae6d836b49cafd189fbcd44ad
Elon Gliksberg <elongliks@gmail.com>
2021-02-27 07:59:36 +0000

    Added some message queue code.

--- msg.c
+++ msg.c
@@ -4,26 +4,39 @@
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/msg.h>
+#include <sys/stat.h>
 
-#define MSQ_KEY 0x41414141
-#define TEXT "Hello World!"
 
 #define ERR_EXIT(err) do { perror(err); exit(EXIT_FAILURE); } while (0)
+
+#define MSQ_KEY 0x41414141
+#define MTYPE 1
+#define BUFSIZE 1024
+#define TEXT "Hello World!"
 
 struct msgbuf {
 	long mtype;       /* message type, must be > 0 */
 	char* mtext;      /* message data */
 };
 
+
 int main() {
 	int msqid;
-	struct msgbuf msg = { 1, TEXT };
+	char buf[BUFSIZE];
+	struct msgbuf msg = { MTYPE, TEXT };
 
-	if ((msqid = msgget(MSQ_KEY, IPC_PRIVATE | IPC_CREAT)) < 0)
+	if ((msqid = msgget(MSQ_KEY, IPC_PRIVATE | IPC_CREAT | S_IRWXU)) < 0)
 		ERR_EXIT("msgget");
 
 	if (msgsnd(msqid, &msg, strlen(TEXT), 0) < 0)
 		ERR_EXIT("msgsnd");
+
+	memset(&msg, 0, sizeof(msg));
+
+	if (msgrcv(msqid, &msg, BUFSIZE, MTYPE, 0) < 0)
+		ERR_EXIT("msgrcv");
+
+	printf("Text: %s\n", msg.mtext);
 
 	if (msgctl(msqid, IPC_RMID, NULL) < 0)
 		ERR_EXIT("msgctl");
